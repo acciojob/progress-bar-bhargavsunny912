58097465d9e90e7e3615003242b1e9929759c84b
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-12-03 12:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+let prev=document.getElementById("prev");
+        let next=document.getElementById("next");
+        let line=document.getElementById("line");
+        let circles=document.getElementsByClassName("circle");
+    
+        var count=0;
+
+        if(count==0){
+            prev.disabled=true;
+        }
+
+        prev.addEventListener("click",()=>{
+            count-=1;
+            if(count==0){
+                prev.disabled=true;
+            }
+            if(count<4){
+                next.disabled=false;
+            }
+
+            for(let i=count+1;i>count;i--){
+                circles[i].classList.remove("active");
+            }
+            line.style.width=`${count*100}px`;
+        });
+
+        next.addEventListener("click",()=>{
+            count+=1;
+            if(count==4){
+                next.disabled=true;
+            }
+            if(count<4){
+                next.disabled=false;
+            }
+            if(count>0){
+                prev.disabled=false;
+            }
+
+            for(let i=0;i<count+1;i++){
+                circles[i].classList.add("active");
+            }
+            line.style.width=`${count*100}px`;
+        })
